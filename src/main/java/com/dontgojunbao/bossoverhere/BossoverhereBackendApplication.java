485dc7718f20762c2b7a780c929b699ff88a14ed
ykh4663 <superho7@gmail.com>
2025-07-29 19:03:41 +0900

    fix: EnableJpaAuditing 추가

--- src/main/java/com/dontgojunbao/bossoverhere/BossoverhereBackendApplication.java
+++ src/main/java/com/dontgojunbao/bossoverhere/BossoverhereBackendApplication.java
@@ -2,8 +2,10 @@ package com.dontgojunbao.bossoverhere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BossoverhereBackendApplication {
 
 	public static void main(String[] args) {
