a89933948fff3e393321d2b4c7b185f0a59ea4b8
ykh4663 <superho7@gmail.com>
2025-07-29 11:56:09 +0900

    init setting

---
+++ src/main/java/com/dontgojunbao/bossoverhere/BossoverhereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dontgojunbao.bossoverhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossoverhereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossoverhereBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dontgojunbao/bossoverhere/BossoverhereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontgojunbao.bossoverhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossoverhereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
