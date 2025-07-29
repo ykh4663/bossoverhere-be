c560ae2c9ad5e519d51fd2a5974ba3b610543157
ykh4663 <superho7@gmail.com>
2025-07-29 12:21:53 +0900

    feat: OAuthProvider 인터페이스 설계

---
+++ src/main/java/com/dontgojunbao/bossoverhere/domain/auth/OauthProvider.java
@@ -0,0 +1,17 @@
+package com.dontgojunbao.bossoverhere.domain.auth;
+
+
+public interface OauthProvider {
+    OauthType getOauthType();
+    OauthClient getOAuthClient();
+
+    default boolean match(String providerName){
+        return providerName.equals(getOauthType().getTypeName());
+    }
+
+    default String getOAuthProviderUserId(String accessToken) {
+        OauthClient client = getOAuthClient();
+        return client.getOAuthProviderUserId(accessToken);
+    }
+
+}
